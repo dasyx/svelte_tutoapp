c3276e0ef06896d8221487c1c09cdc5b2106cac1
Levast David <dasyx@MacBook-Pro-de-Levast.local>
2023-05-09 12:39:06 +0200

    first commit

---
+++ tuto_app/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Products from "./components/Products.svelte";
+  import Users from "./components/Users.svelte";
+</script>
+
+<main>
+  <Products />
+  <Users />
+</main>
+
+<style>
+  
+</style>
---
+++ tuto_app/src/components/Users.svelte
@@ -0,0 +1,13 @@
+<script>
+    const user = {
+        firstName   : 'John',
+        lastName : 'Doe',
+        imageUrl : 'https://picsum.photos/200/300'
+    };
+    function getUser(user) {
+        return user.firstName + ' ' + user.lastName;
+    }
+</script>
+
+<h1>Hello {getUser(user)}</h1>
+<img src={user.imageUrl} alt="sample" />
